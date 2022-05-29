d2ccbea26d2060a3024160f864f972a4240c29bb
ChaouechOlfa <olfa.Chaouech@fsegt.utm.tn>
2022-05-29 01:15:02 +0100

    AddFunctionAddContact

--- app/src/main/java/com/example/androidminiappcontact/AjouterActivity.java
+++ app/src/main/java/com/example/androidminiappcontact/AjouterActivity.java
@@ -3,12 +3,38 @@ package com.example.androidminiappcontact;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class AjouterActivity extends AppCompatActivity {
+    private EditText editAjouterNom;
+    private EditText editAjouterNum;
+    private Button btnAjouter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ajouter);
+        editAjouterNom=(EditText) findViewById(R.id.editAjouterNom);
+        editAjouterNum=(EditText) findViewById(R.id.editAjouterNum);
+        btnAjouter=(Button) findViewById(R.id.btnAjouter);
+
+        btnAjouter.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Contact c=new Contact(editAjouterNom.getText().toString(),editAjouterNum.getText().toString());
+                DatabaseHandler dbh = new DatabaseHandler(AjouterActivity.this);
+                long y = dbh.addContact(c);
+                if (y>0){
+                    Toast.makeText(getApplicationContext(),"ajout effectuer avec succé",Toast.LENGTH_LONG).show();
+                }else{
+                    Toast.makeText(getApplicationContext(),"ajoutn'est pas effectué",Toast.LENGTH_LONG).show();
+                }
+
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/androidminiappcontact/DatabaseHandler.java
+++ app/src/main/java/com/example/androidminiappcontact/DatabaseHandler.java
@@ -1,40 +1,49 @@
 package com.example.androidminiappcontact;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
-public class DatabaseHandler extends SQLiteOpenHelper {
-    public static final int DATABASE_VERSION=1;
-    public static final String DATABASE_NAME="DBContactFavoris.db";
+public class DatabaseHandler  extends SQLiteOpenHelper{
+    private static final int DATABASE_VERSION=1;
+    private static final String DATABASENAME="DBContactFavoris.db";
     private static final String TABLE_NAME="Contact";
     private static final String KEY_ID="id";
-    private static final String KEY_NAME="nom";
-    private static final String KEY_NUMBER="num";
+    private static final String KEY_NAME="Nom";
+    private static final String KEY_NUMBER="Num";
+    private SQLiteDatabase mdb;
 
-    private static final String CREATE_TABLE= "Create table " + TABLE_NAME + " (" +
-            KEY_ID+" "+ "INTEGER PRIMARY KEY autoincrement, " +
-            KEY_NAME+ " TEXT, "+ "KEY_NUMBER"+ "TEXT);";
-
+    private static final String CREATE_TABLE= "create table " +TABLE_NAME+ "( "+
+            KEY_ID+ " "+ "INTEGER PRIMARY KEY autoincrement,"
+             + KEY_NAME+ "TEXT," +KEY_NUMBER+ "TEXT);";
 
     public DatabaseHandler(@Nullable Context context) {
-        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        super(context, DATABASENAME, null, DATABASE_VERSION);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-
         db.execSQL(CREATE_TABLE);
 
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-
-        db.execSQL("drop table " +TABLE_NAME);
+        db.execSQL("drop table "+TABLE_NAME);
         onCreate(db);
 
+    }
+
+    public long addContact(Contact c){
+        mdb=this.getWritableDatabase();
+        ContentValues cv= new ContentValues();
+        cv.put(KEY_NAME,c.getNom());
+        cv.put(KEY_NUMBER,c.getNum());
+        long x= mdb.insert(TABLE_NAME,null,cv);
+        mdb.close();
+        return x;
     }
 }
